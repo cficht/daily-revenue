0ce5e81628d52567eb2f47b92f58b94956a09058
cficht <chris.ficht@gmail.com>
2020-04-25 10:17:05 -0700

    wrote and tested revenue function

---
+++ daily-revenue.js
@@ -0,0 +1,17 @@
+function revenue (transactions) {
+  const newTrans = transactions.reduce((acc, curr) => {
+    const transDate = (new Date(curr.timestamp).toDateString());
+    if(!acc[transDate]) {
+      acc[transDate] = curr.price
+      console.log(curr)
+      console.log(transDate)
+    } else if (acc[transDate])  {
+      acc[transDate] = acc[transDate] + curr.price
+    }
+    return acc;
+  }, {})
+  console.log(newTrans)
+  return newTrans;
+};
+
+module.exports = { revenue };
---
+++ daily-revenue.test.js
@@ -0,0 +1,44 @@
+const { revenue } = require('./daily-revenue.js');
+
+describe('testing daily-revenue.js', () => {
+
+  it('takes in an array of transactions and returns the revenue for each day', () => {
+    const testArray = [
+      {
+        price: 1,
+        timestamp: 1563459038296
+      },
+      {
+        price: 4,
+        timestamp: 1562217298048
+      },
+      {
+        price: 2,
+        timestamp: 1562715645569
+      },
+      {
+        price: 8,
+        timestamp: 1563792873690
+      },
+      {
+        price: 8,
+        timestamp: 1563792873690
+      },
+      {
+        price: 2,
+        timestamp: 1562715645569
+      },
+    ];
+
+    const expectedOutput =
+    {
+      'Thu Jul 18 2019': 1,
+      'Tue Jul 09 2019': 4,
+      'Mon Jul 22 2019': 16,
+      'Wed Jul 03 2019': 4
+    };
+
+    expect(revenue(testArray)).toEqual(expectedOutput);
+  });
+
+})
