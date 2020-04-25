2ae1776ddd59b1b31e8ec5fe6a0903151019faf4
cficht <chris.ficht@gmail.com>
2020-04-25 10:25:42 -0700

    still not passing, but will submit

--- daily-revenue.js
+++ daily-revenue.js
@@ -2,9 +2,7 @@ function revenue (transactions) {
   const newTrans = transactions.reduce((acc, curr) => {
     const transDate = (new Date(curr.timestamp).toDateString());
     if(!acc[transDate]) {
-      acc[transDate] = curr.price
-      console.log(curr);
-      console.log(transDate);
+      acc[transDate] = curr.price;
     } else if (acc[transDate])  {
       acc[transDate] = acc[transDate] + curr.price;
     }
